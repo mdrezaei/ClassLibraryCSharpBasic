a2b479972bda3636fc8a75e192dddb75c678fc4e
m.d.rezaei <m.d.rezaei.official@gmail.com>
2023-08-12 03:42:40 +0330

    First Commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using MyNameSpace;
+using System;
+using System.Globalization;
+
+
+namespace ConsoleApp10_NameSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(DateTime.Now);
+            //PersianCalendar persian = new PersianCalendar();
+            //string PersianDate = persian.GetYear(DateTime.Now).ToString();
+            //Console.WriteLine(PersianDate);
+            //Console.WriteLine(persian.GetYear(DateTime.Now).ToString());
+
+            //ChineseLunisolarCalendar C = new ChineseLunisolarCalendar();
+            //string c = C.GetYear(DateTime.Now).ToString();
+            //Console.WriteLine(c);
+
+            //GregorianCalendar Miladi = new GregorianCalendar();
+
+            //string MiladiCalendar = Miladi.GetYear(DateTime.Now).ToString() + "/" + Miladi.GetMonth(DateTime.Now).ToString() + "/" + Miladi.GetDayOfMonth(DateTime.Now).ToString();
+            //string MCal = Miladi.GetYear(DateTime.Now).ToString() + "/";
+            //Console.WriteLine(MiladiCalendar);
+            //Console.WriteLine(MCal);
+
+            MyProp MP = new MyProp();
+            MP.MyFirstProp = "a";
+            Console.WriteLine(MP.MyFirstProp);
+            
+
+
+
+        }
+    }
+}
